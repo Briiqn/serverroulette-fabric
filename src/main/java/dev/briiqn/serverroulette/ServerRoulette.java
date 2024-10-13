b8bfa44e9491cf3b0aec9b2be832521ba311a936
🤖 <robot@example.org>
2024-10-13 02:04:00 -0400

    Initial commit

---
+++ src/main/java/dev/briiqn/serverroulette/ServerRoulette.java
@@ -0,0 +1,10 @@
+package dev.briiqn.serverroulette;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerRoulette implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/dev/briiqn/serverroulette/client/ServerRouletteClient.java
@@ -0,0 +1,10 @@
+package dev.briiqn.serverroulette.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerRouletteClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
